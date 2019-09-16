6a94f85a7784fed45f24aaf0e5f51b83687a342d
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2019-09-16 14:41:21 +0700

    Menambah Logika implisit

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -15,4 +15,7 @@ public class ExplicitIntentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_explicit_intent);
     }
 
+
+
+
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -2,7 +2,9 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void handleExplicitIntent(View view) {
+        Intent intent =  new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View view) {
+        finish();
+    }
+
+    public void handleImplicitIntent(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
 }
